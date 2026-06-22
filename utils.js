b986e17072723c5b7879efcf41087a95ca9e75b6
AsmodeuZ6532 <sanek06102024@gmail.com>
2026-06-22 13:00:17 +0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,35 @@
+function formatDate(date) {
+    if (!date) return '';
+    const d = new Date(date);
+    return d.toLocaleDateString('ru-RU', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    });
+}
 
+function calcKD(kills, deaths) {
+    if (!deaths || deaths === 0) return kills || 0;
+    return (kills / deaths);
+}
+
+function safeNumber(value, defaultValue = 0) {
+    const num = parseFloat(value);
+    return isNaN(num) ? defaultValue : num;
+}
+
+function generateId() {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
+}
+
+function debounce(func, wait) {
+    let timeout;
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
